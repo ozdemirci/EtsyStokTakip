176dc17dfa3af1d7bd34f101407615ea6f4198fa
ozdemirci <ozdemircihakan@gmail.com>
2025-05-26 23:24:36 +0300

    Remove unused logger imports from app

--- src/main/java/com/yourcompany/etsystoktakip/EtsyStokTakipApplication.java
+++ src/main/java/com/yourcompany/etsystoktakip/EtsyStokTakipApplication.java
@@ -2,8 +2,7 @@ package com.yourcompany.etsystoktakip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class EtsyStokTakipApplication {
