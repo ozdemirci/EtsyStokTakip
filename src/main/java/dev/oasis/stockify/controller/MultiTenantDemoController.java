b377d5ae6364d7db74ab38acfeb7df0d83dc42d3
ozdemirci <ozdemircihakan@gmail.com>
2025-06-26 19:28:06 +0300

    sadmin

--- src/main/java/dev/oasis/stockify/controller/MultiTenantDemoController.java
+++ src/main/java/dev/oasis/stockify/controller/MultiTenantDemoController.java
@@ -10,6 +10,7 @@ import dev.oasis.stockify.repository.ProductCategoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
@@ -27,6 +28,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/api/demo")
+@PreAuthorize("hasRole('SUPER_ADMIN')")
 @RequiredArgsConstructor
 @Slf4j
 public class MultiTenantDemoController {
