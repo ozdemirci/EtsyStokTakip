6fd3131c45fb6456a9dfe38e530faa3ee564f708
Hakan <ozdemircihakan@gmail.com>
2025-06-27 01:01:51 +0300

    feat: show notification badge on all admin pages

---
+++ src/main/java/dev/oasis/stockify/advice/NotificationBadgeAdvice.java
@@ -0,0 +1,31 @@
+package dev.oasis.stockify.advice;
+
+import dev.oasis.stockify.service.StockNotificationService;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice(basePackages = "dev.oasis.stockify.controller")
+public class NotificationBadgeAdvice {
+
+    private final StockNotificationService stockNotificationService;
+
+    public NotificationBadgeAdvice(StockNotificationService stockNotificationService) {
+        this.stockNotificationService = stockNotificationService;
+    }
+
+    @ModelAttribute
+    public void addUnreadNotifications(Model model, HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        if (!uri.startsWith("/admin")) {
+            return;
+        }
+        try {
+            long unread = stockNotificationService.getUnreadNotifications().size();
+            model.addAttribute("unreadNotifications", unread);
+        } catch (Exception e) {
+            model.addAttribute("unreadNotifications", 0);
+        }
+    }
+}
