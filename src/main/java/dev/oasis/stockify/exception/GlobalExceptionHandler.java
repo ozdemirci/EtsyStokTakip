b094e2ab0eabcaf55e1cc8e917892f7c45937b37
Hakan <ozdemircihakan@gmail.com>
2025-06-30 13:02:17 +0300

    Improve error handling

--- src/main/java/dev/oasis/stockify/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/oasis/stockify/exception/GlobalExceptionHandler.java
@@ -12,7 +12,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
@@ -21,7 +21,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // Custom exception class (you'll need to create this)
